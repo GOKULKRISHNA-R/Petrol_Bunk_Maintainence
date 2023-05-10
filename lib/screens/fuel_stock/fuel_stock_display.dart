d4ccdf92e97963b9fbed3b8b445d60ac49228ef2
Naveenkumar S E <naveen42moon@gmail.com>
2023-05-10 10:32:23 +0530

    Update fuel_stock_display.dart

--- lib/screens/fuel_stock/fuel_stock_display.dart
+++ lib/screens/fuel_stock/fuel_stock_display.dart
@@ -74,79 +74,82 @@ class FuelStockDisplay extends StatelessWidget {
           SizedBox(
             height: Get.height * 0.04,
           ),
-          Container(
-            padding: EdgeInsets.only(
-                top: Get.height * 0.01, bottom: Get.height * 0.01),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Update products",
-                  style: TextStyle(fontSize: 25),
-                ),
-              ],
-            ),
-          ),
-          // Divider(
-          //   color: Colors.grey,
-          //   height: 1,
-          //   thickness: 1,
-          // ),
-          SizedBox(
-            height: Get.height * 0.05,
-          ),
-          TextField(
-            // controller: calculatorController.todayDiesel,
-            decoration: InputDecoration(
-                isDense: true,
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
-                counterText: 'Petrol',
-                hintText: "Enter the amount of petrol added (in lit)"),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          ),
-          SizedBox(
-            height: Get.height * 0.04,
-          ),
-          TextField(
-            // controller: calculatorController.todayDiesel,
-            decoration: InputDecoration(
-              isDense: true,
-              contentPadding: EdgeInsets.symmetric(
-                  horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
-              counterText: 'Diesel',
-              hintText: "Enter the amount of diesel added (in lit)",
-            ),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          ),
-          SizedBox(
-            height: Get.height * 0.04,
-          ),
-          TextField(
-            // controller: calculatorController.todayDiesel,
-            decoration: InputDecoration(
-                isDense: true,
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
-                counterText: 'Oil',
-                hintText: "Enter the amount of oil added (in lit)"),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          ),
-
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor:
-                  Color.fromARGB(255, 154, 46, 46), // background (button) color
-              foregroundColor: Colors.white, // foreground (text) color
-            ),
-            onPressed: () {
-              // int petrolCost =
-            },
-            child: const Text('Update'),
-          ),
         ])));
   }
 }
+
+
+
+  // Container(
+  //           padding: EdgeInsets.only(
+  //               top: Get.height * 0.01, bottom: Get.height * 0.01),
+  //           child: Row(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [
+  //               Text(
+  //                 "Update products",
+  //                 style: TextStyle(fontSize: 25),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //         // Divider(
+  //         //   color: Colors.grey,
+  //         //   height: 1,
+  //         //   thickness: 1,
+  //         // ),
+  //         SizedBox(
+  //           height: Get.height * 0.05,
+  //         ),
+  //         TextField(
+  //           // controller: calculatorController.todayDiesel,
+  //           decoration: InputDecoration(
+  //               isDense: true,
+  //               contentPadding: EdgeInsets.symmetric(
+  //                   horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
+  //               counterText: 'Petrol',
+  //               hintText: "Enter the amount of petrol added (in lit)"),
+  //           keyboardType: TextInputType.number,
+  //           inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+  //         ),
+  //         SizedBox(
+  //           height: Get.height * 0.04,
+  //         ),
+  //         TextField(
+  //           // controller: calculatorController.todayDiesel,
+  //           decoration: InputDecoration(
+  //             isDense: true,
+  //             contentPadding: EdgeInsets.symmetric(
+  //                 horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
+  //             counterText: 'Diesel',
+  //             hintText: "Enter the amount of diesel added (in lit)",
+  //           ),
+  //           keyboardType: TextInputType.number,
+  //           inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+  //         ),
+  //         SizedBox(
+  //           height: Get.height * 0.04,
+  //         ),
+  //         TextField(
+  //           // controller: calculatorController.todayDiesel,
+  //           decoration: InputDecoration(
+  //               isDense: true,
+  //               contentPadding: EdgeInsets.symmetric(
+  //                   horizontal: Get.width * 0.03, vertical: Get.height * 0.01),
+  //               counterText: 'Oil',
+  //               hintText: "Enter the amount of oil added (in lit)"),
+  //           keyboardType: TextInputType.number,
+  //           inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+  //         ),
+
+  //         ElevatedButton(
+  //           style: ElevatedButton.styleFrom(
+  //             backgroundColor:
+  //                 Color.fromARGB(255, 154, 46, 46), // background (button) color
+  //             foregroundColor: Colors.white, // foreground (text) color
+  //           ),
+  //           onPressed: () {
+  //             // int petrolCost =
+  //           },
+  //           child: const Text('Update'),
+  //         ),
